b060c66a52ac5752386e10ca281ccd4611f26789
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-16 10:12:29 -0400

    first_commit3

--- src/CompleteInitialization.java
+++ src/CompleteInitialization.java
@@ -3,16 +3,13 @@ public class CompleteInitialization {
         int workOutDays = 10;
         double health = 8.5;
         boolean powerUp = true;
-        int lastAssment = 10;
-        int healthScale = 10;
-
 
 
         System.out.println("Number of days: " + workOutDays);
         System.out.println("Health Scale: "+ health);
         System.out.println("Are you ready to power up?: " + powerUp);
-        System.out.println("Total Assessment: It has been " + lastAssment + " days since you've started working out");
-        System.out.println("You evaluate at" + healthScale+ "out of " + healthScale + "on the health scale.");
-        System.out.println("It is true that you are read to power up your workout!");
+        System.out.println("Total Assessment: It has been " + workOutDays + " days since you've started working out");
+        System.out.println("You evaluate at" + health + "out of " + workOutDays + "on the health scale." + "\nIt is true that you are read to power up your workout!");
+
     }
 }
